ab147beebcb443c50cbfa002e9468d3f89b03c5a
Ahmed Mabrouck <ahmed.mabrouck@outlook.com>
2015-12-07 15:39:16 +0200

    WinRT Samples.

---
+++ MVVM Sample/PdfViewerSample/PdfViewerSample.Shared/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PdfViewerSample.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Uri pdf;
+
+        public Uri Pdf
+        {
+            get { return pdf; }
+            set { pdf = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Pdf))); }
+        }
+
+        public MainViewModel()
+        {
+            Pdf = new Uri("ms-appx:///sample.pdf");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
